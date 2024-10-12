3531ba2e70d658ad2397ed295be307a5844a5de9
Yixun Wang <104146635+FreeWillThorn@users.noreply.github.com>
2024-10-12 11:23:49 +0200

    change dataset structure

--- configs/_base_/datasets/coco-stuff164k.py
+++ configs/_base_/datasets/coco-stuff164k.py
@@ -1,14 +1,17 @@
 # dataset settings
 dataset_type = 'COCOStuffDataset'
-data_root = 'data/coco_stuff164k'
-crop_size = (512, 512)
+data_root = 'data/TumorSeg_brain'
+#crop_size = (512, 512)
+crop_size = (640, 640)
 train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations'),
     dict(
-        type='RandomResize',
-        scale=(2048, 512),
-        ratio_range=(0.5, 2.0),
+        #type='RandomResize',
+        #scale=(2048, 512),
+        #ratio_range=(0.5, 2.0),
+        type='Resize',
+        scale=(640, 640),
         keep_ratio=True),
     dict(type='RandomCrop', crop_size=crop_size, cat_max_ratio=0.75),
     dict(type='RandomFlip', prob=0.5),
@@ -17,7 +20,10 @@ train_pipeline = [
 ]
 test_pipeline = [
     dict(type='LoadImageFromFile'),
-    dict(type='Resize', scale=(2048, 512), keep_ratio=True),
+    dict(type='Resize',
+         #scale=(2048, 512),
+        scale=(640, 640),
+        keep_ratio=True),
     # add loading annotation after ``Resize`` because ground truth
     # does not need to do resize data transform
     dict(type='LoadAnnotations'),
@@ -48,7 +54,7 @@ train_dataloader = dict(
         type=dataset_type,
         data_root=data_root,
         data_prefix=dict(
-            img_path='images/train2017', seg_map_path='annotations/train2017'),
+            img_path='images/train', seg_map_path='annotations/train_annotations.coco'),
         pipeline=train_pipeline))
 val_dataloader = dict(
     batch_size=1,
@@ -59,9 +65,21 @@ val_dataloader = dict(
         type=dataset_type,
         data_root=data_root,
         data_prefix=dict(
-            img_path='images/val2017', seg_map_path='annotations/val2017'),
+            img_path='images/valid', seg_map_path='annotations/valid_annotations.coco'),
         pipeline=test_pipeline))
-test_dataloader = val_dataloader
+#test_dataloader = val_dataloader
+#since i have do have test set
+test_dataloader = dict(
+    batch_size=1,
+    num_workers=4,
+    persistent_workers=True,
+    sampler=dict(type='DefaultSampler', shuffle=False),
+    dataset=dict(
+        type=dataset_type,
+        data_root=data_root,
+        data_prefix=dict(
+            img_path='images/test', seg_map_path='annotations/test_annotations.coco'),
+        pipeline=test_pipeline))
 
 val_evaluator = dict(type='IoUMetric', iou_metrics=['mIoU'])
 test_evaluator = val_evaluator
