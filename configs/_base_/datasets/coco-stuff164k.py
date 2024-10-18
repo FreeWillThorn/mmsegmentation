7a129d015825004ef65a7410757132f1234ff5d8
Yixun Wang <104146635+FreeWillThorn@users.noreply.github.com>
2024-10-18 18:25:44 +0200

    batchsize = 8

--- configs/_base_/datasets/coco-stuff164k.py
+++ configs/_base_/datasets/coco-stuff164k.py
@@ -46,7 +46,7 @@ tta_pipeline = [
         ])
 ]
 train_dataloader = dict(
-    batch_size=2,
+    batch_size=8,
     num_workers=4,
     persistent_workers=True,
     sampler=dict(type='InfiniteSampler', shuffle=True),
